d69a0ddfb0ae114d21e9ca4f98fe5a3600732e9c
adrielfsc <adrielcavaleiro@gmail.com>
2025-06-12 01:09:41 -0300

    first commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,15 @@
+export default function TabButton({children, onSelect}){
+    // document.querySelector('button').addEventListener('click',()=>{});
+    // function handleClick(){
+    //     console.log("Hello world!")
+    // }
+
+    // só se usa o nome da função, sem os () pq a função tá sendo passada como um valor, 
+    // nesse caso ela só vai ser executada pelo próprio react quando o evento for inicializado
+    return( 
+        <li>
+            
+            <button onClick={onSelect}>{children}</button>
+        </li>
+    );
+}
