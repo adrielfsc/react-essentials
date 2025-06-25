ce8dcd573ef718b26f11fb8e3f342c64ee93fb31
adrielfsc <adrielcavaleiro@gmail.com>
2025-06-25 01:35:52 -0300

    Tabs element, passing jsx elements as a value in buttons, setting elements dynamically ButtonsContainer

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton";
 import Section from "./Section";
+import Tabs from "./Tabs";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -30,14 +31,16 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <h2>Examples</h2>
-      <menu>
+      <Tabs 
+      buttonsContainer="menu"  
+      buttons={<>
         <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
         <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
         <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
         <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
-      </menu>
-      {/* (!selectedTopic && tabContent) */}
-      {tabContent}
+      </>}>
+        {tabContent}
+      </Tabs>
     </Section>
   );
 }
